ed7b4d481bbdd06985064d64e4cf3f7d08c20b93
sunjinshuai <s_mike@163.com>
2017-02-08 22:50:36 +0800

    UICollectionView实现轮播图
    UICollectionView实现轮播图

--- BannerCycleView/BannerViewCell.m
+++ BannerCycleView/BannerViewCell.m
@@ -8,12 +8,15 @@
 
 #import "BannerViewCell.h"
 
+#define random(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)/255.0]
+#define randomColor random(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
+
 @implementation BannerViewCell
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
-        self.contentView.backgroundColor = [UIColor redColor];
+        self.contentView.backgroundColor = randomColor;
     }
     return self;
 }
